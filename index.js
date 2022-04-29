d81b2f7c213ee871d8a67349816104b4dec5fb91
ArafatMunna <arafatmunna000@gmail.com>
2022-04-29 21:42:10 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ye9by.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+
 app.get('/', (req, res) => {
     res.send('Fruits Warehouse Running');
 });
