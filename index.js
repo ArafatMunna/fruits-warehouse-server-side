aaf9bc9a33a6be6c9af6151333031d02f4b019db
ArafatMunna <arafatmunna000@gmail.com>
2022-04-30 07:03:18 +0600

    delete method added

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ async function run() {
                 message: `Successfully Updated!`,
             });
         });
+
+        //DELETE
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
     } finally {
     }
 }
